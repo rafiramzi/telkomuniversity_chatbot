c29e9d5364205ea393af47885d97f180d70fd8ee
Rafi Ramzi <rafiramzi@Rafis-MacBook-Air.local>
2025-12-02 02:10:00 +0700

    Update Category Algorythm

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import OllamaChatView, UploadPDFView, ChatBot
+from .views import UploadPDFView, ChatBot
 
 urlpatterns = [
     # path('chat/', OllamaChatView.as_view(), name='ollama_chat'),
--- ollama_api/urls.py
+++ ollama_api/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import OllamaChatView
+# from core.views import OllamaChatView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
